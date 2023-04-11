69434c0c55328e35c3527ee7be6f38404d0985e7
Sandeep kumar singh <sandeeprj793@gmail.com>
2023-04-11 22:41:48 +0530

    [Sandeep] Modify Check for a Option No Play, Ladder or Snake

--- SnakeLadderGame/Program.cs
+++ SnakeLadderGame/Program.cs
@@ -10,52 +10,38 @@ namespace SnakeLadderGame
     {
         static void Main(string[] args)
         {
-            using System;
+            const int WinPosition = 100;
+            int position = 0;
 
-class Program
-        {
-            static void Main(string[] args)
+            Random random = new Random();
+
+            while (position < WinPosition)
             {
-                const int WinPosition = 100;
-                int position = 0;
-                int numRolls = 0;
+                // UC1: Player rolls the die to get a number between 1 to 6
+                int dieRoll = random.Next(1, 7);
 
-                Random random = new Random();
+                // UC2: Player checks for an option
+                int option = random.Next(0, 3); // 0: No Play, 1: Ladder, 2: Snake
 
-                while (position < WinPosition)
+                switch (option)
                 {
-                    int dieRoll = random.Next(1, 7);
-                    int option = random.Next(0, 3); 
-
-                    switch (option)
-                    {
-                        case 0:
-                            // No Play
-                            break;
-                        case 1:
-                            // Ladder
-                            position += dieRoll;
-                            break;
-                        case 2:
-                            // Snake
-                            position -= dieRoll;
-                            break;
-                    }
-
-                    // Ensure position is within bounds
-                    position = Math.Max(0, position);
-                    position = Math.Min(WinPosition, position);
-
-                    numRolls++;
-
-                    Console.WriteLine($"Roll #{numRolls}: Rolled a {dieRoll}, landed on {position}");
+                    case 0:
+                        break;
+                    case 1:
+                        position += dieRoll;
+                        break;
+                    case 2:
+                        position -= dieRoll;
+                        break;
                 }
+                position = Math.Max(0, position);
+                position = Math.Min(WinPosition, position);
 
-                Console.WriteLine($"Congratulations, you won in {numRolls} rolls!");
-                Console.ReadLine();
+                Console.WriteLine($"Rolled a {dieRoll}, landed on {position}");
             }
-        }
 
+            Console.WriteLine("You won!");
+            Console.ReadLine();
+        }
     }
-}
 }
