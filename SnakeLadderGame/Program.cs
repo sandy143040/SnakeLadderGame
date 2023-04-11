112dc58c8ddc7b5f2e3fe1c66d0857c2de54a5f6
Sandeep kumar singh <sandeeprj793@gmail.com>
2023-04-11 23:56:46 +0530

    [Sandeep] Modify Play the game with 2 Player

--- SnakeLadderGame/Program.cs
+++ SnakeLadderGame/Program.cs
@@ -11,42 +11,93 @@ namespace SnakeLadderGame
         static void Main(string[] args)
         {
             Console.WriteLine("Welcom to Snake Ladder Game");
-            int position = 0;
-            int diceRolls = 0;
+            int position1 = 0;
+            int position2 = 0;
+            int diceRolls1 = 0;
+            int diceRolls2 = 0;
+            int currentPlayer = 1;
+            bool ladderRolled = false;
             Random rand = new Random();
 
-            while (position != 100)
+            while (position1 < 100 && position2 < 100)
             {
                 int dice = rand.Next(1, 7);
                 int option = rand.Next(0, 3);
-                diceRolls++;
 
-                switch (option)
+                if (currentPlayer == 1)
                 {
-                    case 0:
-                        // No Play
-                        break;
-                    case 1:
-                        // Ladder
-                        if (position + dice <= 100)
-                        {
-                            position += dice;
-                        }
-                        break;
-                    case 2:
-                        // Snake
-                        position -= dice;
-                        if (position < 0)
-                        {
-                            position = 0;
-                        }
-                        break;
-                }
+                    diceRolls1++;
 
-                Console.WriteLine("Dice Roll: " + dice + " | Option: " + option + " | Position: " + position);
+                    switch (option)
+                    {
+                        case 0:
+                            // No Play
+                            break;
+                        case 1:
+                            // Ladder
+                            position1 += dice;
+                            ladderRolled = true;
+                            break;
+                        case 2:
+                            // Snake
+                            position1 -= dice;
+                            if (position1 < 0)
+                            {
+                                position1 = 0;
+                            }
+                            break;
+                    }
+
+                    Console.WriteLine("Player 1 | Dice Roll: " + dice + " | Option: " + option + " | Position: " + position1);
+
+                    if (!ladderRolled)
+                    {
+                        currentPlayer = 2;
+                    }
+                    ladderRolled = false;
+                }
+                else
+                {
+                    diceRolls2++;
+
+                    switch (option)
+                    {
+                        case 0:
+                            // No Play
+                            break;
+                        case 1:
+                            // Ladder
+                            position2 += dice;
+                            ladderRolled = true;
+                            break;
+                        case 2:
+                            // Snake
+                            position2 -= dice;
+                            if (position2 < 0)
+                            {
+                                position2 = 0;
+                            }
+                            break;
+                    }
+
+                    Console.WriteLine("Player 2 | Dice Roll: " + dice + " | Option: " + option + " | Position: " + position2);
+
+                    if (!ladderRolled)
+                    {
+                        currentPlayer = 1;
+                    }
+                    ladderRolled = false;
+                }
             }
 
-            Console.WriteLine("Congratulations! You won the game in " + diceRolls + " dice rolls.");
+            if (position1 >= 100)
+            {
+                Console.WriteLine("Congratulations! Player 1 won the game in " + diceRolls1 + " dice rolls.");
+            }
+            else
+            {
+                Console.WriteLine("Congratulations! Player 2 won the game in " + diceRolls2 + " dice rolls.");
+            }
             Console.ReadLine();
         }
     }
