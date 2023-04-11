1688cec4747d66ec1e706952bc286fba6cdabd9b
Sandeep kumar singh <sandeeprj793@gmail.com>
2023-04-11 18:47:22 +0530

    [Sandeep] Modify Play with Single Player at start postion Zero

--- SnakeLadderGame/Program.cs
+++ SnakeLadderGame/Program.cs
@@ -11,6 +11,37 @@ namespace SnakeLadderGame
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Snake Ladder Game");
+            int position = 0; // starting position
+            Random rand = new Random();
+
+            while (position < 100)
+            { // game loop
+              // Roll the die and get a random number between 1 and 6
+                int roll = rand.Next(1, 7);
+                Console.WriteLine("You rolled a " + roll);
+
+                // Check for options (No Play, Ladder, or Snake)
+                int option = rand.Next(0, 3);
+                switch (option)
+                {
+                    case 0:
+                        Console.WriteLine("No Play. You stay at position " + position);
+                        break;
+                    case 1:
+                        Console.WriteLine("You climbed a ladder to position " + (position + roll));
+                        position += roll;
+                        break;
+                    case 2:
+                        Console.WriteLine("You slid down a snake to position " + (position - roll));
+                        position -= roll;
+                        if (position < 0) position = 0; // ensure position doesn't go below 0
+                        break;
+                }
+
+                Console.WriteLine("Your current position is " + position);
+            }
+
+            Console.WriteLine("Congratulations, you won!");
             Console.ReadLine();
         }
     }
