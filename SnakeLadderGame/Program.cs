a1d3dfdc88fcb6efede3bf098221283eede95198
Sandeep kumar singh <sandeeprj793@gmail.com>
2023-04-11 23:12:30 +0530

    [Sandeep] Modify Restart or play again

--- SnakeLadderGame/Program.cs
+++ SnakeLadderGame/Program.cs
@@ -17,23 +17,28 @@ namespace SnakeLadderGame
 
             while (position < WinPosition)
             {
-                // UC1: Player rolls the die to get a number between 1 to 6
+                //Player rolls the die to get a number between 1 to 6
                 int dieRoll = random.Next(1, 7);
 
-                // UC2: Player checks for an option
+                //Player checks for an option
                 int option = random.Next(0, 3); // 0: No Play, 1: Ladder, 2: Snake
 
                 switch (option)
                 {
                     case 0:
+                        // No Play
                         break;
                     case 1:
+                        // Ladder
                         position += dieRoll;
                         break;
                     case 2:
+                        // Snake
                         position -= dieRoll;
                         break;
                 }
+
+                // Ensure position is within bounds
                 position = Math.Max(0, position);
                 position = Math.Min(WinPosition, position);
 
@@ -41,7 +46,15 @@ namespace SnakeLadderGame
             }
 
             Console.WriteLine("You won!");
+
+            // Reset game if player wants to play again
+            Console.WriteLine("Do you want to play again? (y/n)");
+            string answer = Console.ReadLine();
+            if (answer.ToLower() == "y")
+            {
+                Main(args); // Recursive call to start a new game
+            }
             Console.ReadLine();
-        }
+        }    
     }
 }
